77cb129d5b1cf0db718dfed005f60d3a7f838555
Phantomhive <82524939+vitorPhantomhive@users.noreply.github.com>
2022-08-26 14:16:40 -0300

    soma funcionando agora falta todo o resto

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 // Função que mostra o valor do input num alert
 function mostrarValor() {
-    alert(document.getElementById("meu-input").value);
+    //alert(document.getElementById("meu-submit").value);
+    var n1 = document.getElementById("number1").value;
+    var n2 = document.getElementById("number2").value;
+
+    const n1conv = parseInt(n1);
+    const n2conv = parseInt(n2);
+
+
+    var n3 = (n1conv + n2conv);
+
+alert(n3);
+
+
+   
 }
 
