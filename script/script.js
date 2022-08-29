2ae730511efcfbcc8a4d3ffcbcbd67e4e9b4a0ac
VitorPhantomhive <vitorkhaled47@gmail.com>
2022-08-29 20:11:00 -0300

    terminando a lógica da calculadora pegando e processando os dados do input

--- script/script.js
+++ script/script.js
@@ -1,18 +1,55 @@
 // Função que mostra o valor do input num alert
-function mostrarValor() {
+function Somar() {
     //alert(document.getElementById("meu-submit").value);
     var n1 = document.getElementById("number1").value;
     var n2 = document.getElementById("number2").value;
 
-    const n1conv = parseInt(n1);
-    const n2conv = parseInt(n2);
+    const n1conv = parseFloat(n1);
+    const n2conv = parseFloat(n2);
 
 
     var n3 = (n1conv + n2conv);
 
 alert(n3);
-
-
-   
 }
 
+function Subtrair(){
+
+    var n1 = document.getElementById("number1").value;
+    var n2 = document.getElementById("number2").value;
+
+    const n1conv = parseFloat(n1);
+    const n2conv = parseFloat(n2);
+
+    const n3 = n1conv - n2conv;
+
+    alert(n3);
+}
+
+function Dividir(){
+    
+    const n1 = document.getElementById("number1").value;
+    const n2 = document.getElementById("number2").value;
+
+    const n1conv = parseFloat(n1);
+    const n2conv = parseFloat(n2);
+
+    const n3 = (n1conv / n2conv);
+
+    alert(n3);
+}
+
+
+
+function Multiplicar(){
+    
+    const n1 = document.getElementById("number1").value;
+    const n2 = document.getElementById("number2").value;
+
+    const n1conv = parseFloat(n1);
+    const n2conv = parseFloat(n2);
+
+    const n3 = (n1conv * n2conv);
+
+    alert(n3);
+}
